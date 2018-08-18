e5f6d2d0591a28bdfcf6f520d321bf6c44d5179d
Joe Raven <joakim.ringstad.raavi@gmail.com>
2018-08-18 22:47:32 +0200

    Add files via upload

---
+++ AngularRouter/app.js
@@ -0,0 +1,47 @@
+// app.js
+
+// define our application and pull in ngRoute and ngAnimate
+angular.module('animateApp', ['ngRoute', 'ngAnimate'])
+
+// ROUTING ===============================================
+// set our routing for this application
+// each route will pull in a different controller
+.config(function($routeProvider) {
+
+    $routeProvider
+
+        // home page
+        .when('/', {
+            templateUrl: './pages/main-page.htm',
+            controller: 'main-page-controller'
+        })
+
+        // about page
+        .when('/first-page', {
+            templateUrl: './pages/first-page.htm',
+            controller: 'first-page-controller'
+        })
+
+        // contact page
+        .when('/second-page', {
+            templateUrl: './pages/second-page.htm',
+            controller: 'second-page-controller'
+        });
+
+})
+
+// CONTROLLERS ============================================
+// home page controller
+.controller('main-page-controller', function($scope) {
+    $scope.pageClass = 'main-page';
+})
+
+// about page controller
+.controller('first-page-controller', function($scope) {
+    $scope.pageClass = 'first-page';
+})
+
+// contact page controller
+.controller('second-page-controller', function($scope) {
+    $scope.pageClass = 'second-page';
+});
